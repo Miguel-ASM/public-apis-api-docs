930932d3ed401ae8ddd51fe8b8aa729c9cbe1fe3
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2023-10-08 11:09:26 +0200

    search section translations

--- src/components/react/Search.jsx
+++ src/components/react/Search.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import usePublicApis from "./hooks/use-public-apis";
 import ReactPaginate from "react-paginate";
+import { useTranslation } from "react-i18next";
 
 import "./Search.css";
 
 export default function Search({ searchExamples = [] }) {
+  const { t } = useTranslation();
   const [submitEnabled, setsubmitEnabled] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const { apiSearchResponse, search, paginate, isSearching, isPaginating } =
@@ -36,7 +38,7 @@ export default function Search({ searchExamples = [] }) {
   return (
     <section id="search-section" className="flex w-full flex-col">
       <h2 className="mb-4 text-3xl font-bold tracking-tight text-gray-900">
-        Search
+        {t("search.h2")}
       </h2>
       <form
         className="flex w-full flex-col gap-x-4 gap-y-4 sm:flex-row"
@@ -46,10 +48,10 @@ export default function Search({ searchExamples = [] }) {
           <input
             onChange={onSearchInputChange}
             formNoValidate
-            title="search"
+            title={t("search.search_input_title")}
             name="query"
             className="h-10 w-full rounded-lg border-2 border-indigo-600 px-3"
-            placeholder="Search what kind of api you need"
+            placeholder={t("search.search_input_placeholder")}
             id="search"
             type="text"
           />
@@ -61,7 +63,7 @@ export default function Search({ searchExamples = [] }) {
             className="h-10 w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-gray-400"
             type="submit"
           >
-            Search
+            {t("search.search_submit_button")}
           </button>
         </div>
       </form>
