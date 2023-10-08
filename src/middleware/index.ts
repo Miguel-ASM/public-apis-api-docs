1a5369861bf6ed8478e301124b5c9281410ad71e
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2023-10-08 10:22:37 +0200

    Fix internationalizaton for production

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,11 +1,13 @@
-import { sequence } from "astro/middleware";
+import { sequence, defineMiddleware } from "astro/middleware";
 import { i18nMiddleware, getLocale } from "astro-i18n-aut";
 import i18next from "i18next";
 
-async function setLangI18next(context, next) {
-  const locale = getLocale(new URL(context.request.url));
+const setLangI18next = defineMiddleware(async (context, next) => {
+  const pathName = new URL(context.url).pathname;
+  const [pathNameWithoutExtension] = pathName.split(".");
+  const locale = getLocale(pathNameWithoutExtension);
   await i18next.changeLanguage(locale);
   return await next();
-}
+});
 
 export const onRequest = sequence(i18nMiddleware, setLangI18next);
